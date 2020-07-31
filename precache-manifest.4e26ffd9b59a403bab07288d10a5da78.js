890527845c1da86c98be3b772a332e52f90f9007
Sean <seanho96@yahoo.com>
2020-07-31 21:51:44 +0800

    Updates

---
+++ precache-manifest.4e26ffd9b59a403bab07288d10a5da78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cdc8f03eee4ecbee8ed",
+    "url": "/trello-clone/static/css/main.2af04302.chunk.css"
+  },
+  {
+    "revision": "9cdc8f03eee4ecbee8ed",
+    "url": "/trello-clone/static/js/main.9cdc8f03.chunk.js"
+  },
+  {
+    "revision": "77a8e5d759d4dcebcb23",
+    "url": "/trello-clone/static/js/runtime~main.77a8e5d7.js"
+  },
+  {
+    "revision": "ec91239408a8f813081c",
+    "url": "/trello-clone/static/js/2.ec912394.chunk.js"
+  },
+  {
+    "revision": "fffcd17850848ff61d30abe9efbf8b9e",
+    "url": "/trello-clone/index.html"
+  }
+];
